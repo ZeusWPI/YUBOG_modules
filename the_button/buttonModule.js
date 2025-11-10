dbd246084673fc47427f936ce0aa3e330aea764d
landgeet <lander.degeeter@ugent.be>
2025-11-10 17:54:48 +0100

    changes from last time

--- the_button/buttonModule.js
+++ the_button/buttonModule.js
@@ -1,92 +1,30 @@
 
-import BombModule from "https://hannes-dev.github.io/YUBOG/ModuleLib.js"
+import BombModule from "./YUBOG/ModuleLib.js"
 
 
-const bombModule = new BombModule();
+const module = new BombModule();
 
-const button = document.getElementById("Knop");
 
 const randomNum = Math.floor(Math.random() * 4);
-
-let indicatorColor;
-
-let buttonColor;
-
-let buttonText;
-
-let pressed;
+//returns array numbers in timer
+export function timer(module) {
+     ms = module.getTimeRemainingMs();
+     let minutes = Math.floor(ms / 60000);
+     let seconds = Math.floor((ms - minutes * 60000) / 1000);
+     if (seconds < 10) {
+         seconds = "0" + seconds;
+     }
+     return minutes + seconds;
+}
         
 export function clicked() {
      const fillColors = ["yellow","blue","white","red"];
-     indicatorColor = fillColors[randomNum]
-     document.querySelector('#rect').setAttribute("fill", indicatorColor);
-     pressed = new Date().getTime();
-     button.addEventListener("mouseout", buttonReleased);
+     document.querySelector('#indicator rect').setAttribute("fill", fillColors[randomNum]);
 }
-
-function vowelTest(s) {
-    return (/^.*[aeiou].*$/i).test(s);
-}
-
 
 export function buttonReleased() {
-    document.querySelector('#rect').setAttribute("fill", "grey")
-    console.log(bombModule.getBombTimeString())
-    ruleChecks((new Date().getTime()) - pressed, bombModule.getBombTimeString()) ? bombModule.sendSolve() : bombModule.sendStrike();
-
-    button.removeEventListener("mouseout", buttonReleased);
+    document.querySelector('#indicator rect').setAttribute("fill", "grey")
 }
-
-function ruleChecks(holdTime, bombTime){
-    const heldDown = ((new Date().getTime()) - pressed) > 150
-
-    console.log(buttonColor);
-    console.log(buttonText);
-    console.log(bombModule.getSerialNumber())
-    console.log(vowelTest(bombModule.getSerialNumber()))
-
-    if(buttonColor === "blue" && buttonText === "Abort"){
-        if(heldDown){
-            return releasingHeldButton(bombTime);
-        }
-    }else if(bombModule.getTotalBatteries() && buttonText === "Detonate"){
-        return !heldDown;
-    }else if(buttonColor === "white" && vowelTest(bombModule.getSerialNumber())){
-        if(heldDown){
-            return releasingHeldButton(bombTime);
-        }
-    }else if(bombModule.getTotalBatteries() > 2 && bombModule.getTotalModuleAmount() >= 6) {
-        return !heldDown;
-    }else if(buttonColor === "yellow"){
-        if(heldDown){
-            return releasingHeldButton(bombTime);
-        }
-    }else if(buttonColor === "red" && buttonText === "Hold"){
-        return !heldDown;
-    }else{
-        if(heldDown){
-            return releasingHeldButton(bombTime);
-        }
-    }
-
-    return false;
-
-}
-
-function releasingHeldButton(bombTime){
-    const dict = {
-        "blue": '4',
-        "white": '1',
-        "yellow": '5'
-    }
-    if (indicatorColor in dict){
-        return bombTime.includes(dict[indicatorColor]);
-    }
-    //other color
-    return bombTime.includes("3")
-}
-
-
 // Function to change the button color
 function changeButtonColor() {
      const bgcolors = ["yellow","red","white","blue", "black"];
@@ -96,14 +34,13 @@ function changeButtonColor() {
      const button = document.getElementById("Knop");
 
      if (button){
-         buttonColor = bgcolors[randomNumber]
-         button.style.backgroundColor = buttonColor;
-         button.style.color = colors[randomNumber];
-         button.style.border = "2px solid " + bgcolors[randomNumber];
+          button.style.backgroundColor = bgcolors[randomNumber];
+          button.style.color = colors[randomNumber];
+          button.style.border = "2px solid " + bgcolors[randomNumber];
      
-         const arr = ["Press", "Hold", "Abort", "Detonate"];
-         buttonText = arr[Math.floor(Math.random() * arr.length)];
-         button.textContent = buttonText;
+          const arr = ["Press", "Hold", "Abort", "Detonate"];
+          const randomValue = arr[Math.floor(Math.random() * arr.length)];
+          button.textContent = randomValue;
      }
 }
 
@@ -115,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (button) {
         button.addEventListener("mousedown", clicked);
         button.addEventListener("mouseup", buttonReleased);
+        button.addEventListener("mouseout", buttonReleased);
     }
 });
   
