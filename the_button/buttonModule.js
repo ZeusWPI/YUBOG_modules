c39d080569dc3d6c5c1feb2e1d1adc573bdc34e7
landgeet <lander.degeeter@ugent.be>
2025-11-10 21:19:37 +0100

    implement releaseHeldButton

--- the_button/buttonModule.js
+++ the_button/buttonModule.js
@@ -1,46 +1,82 @@
 
-import BombModule from "./YUBOG/ModuleLib.js"
+import BombModule from "../../YUBOG/ModuleLib.js"
 
 
 const module = new BombModule();
 
+let isPressed = false;
+
+let bgColor;
+
+let text;
+
+let stripColor;
+
+let clickTimeMs;
+
+let releaseTime;
+
+
 
 const randomNum = Math.floor(Math.random() * 4);
-//returns array numbers in timer
-export function timer(module) {
-     ms = module.getTimeRemainingMs();
-     let minutes = Math.floor(ms / 60000);
-     let seconds = Math.floor((ms - minutes * 60000) / 1000);
-     if (seconds < 10) {
-         seconds = "0" + seconds;
-     }
-     return minutes + seconds;
-}
-        
+   
 export function clicked() {
-     const fillColors = ["yellow","blue","white","red"];
-     document.querySelector('#indicator rect').setAttribute("fill", fillColors[randomNum]);
+    isPressed = true;
+    
+    document.querySelector('#indicator rect').setAttribute("fill", stripColor);
+    clickTimeMs = module.getTimeRemainingMs();
+    console.log(module.getTimeRemainingMs());
 }
 
 export function buttonReleased() {
-    document.querySelector('#indicator rect').setAttribute("fill", "grey")
+    if (!isPressed) return;
+    releaseTime = module.getBombTimeString();
+    const batteries = module.getTotalBatteries();
+    const isHeld = (clickTimeMs - module.getTimeRemainingMs()) > 250;
+    console.log(releaseHeldButton(isHeld));
+
+    if(bgColor === "blue" && text === "Abort" && isHeld) {
+
+    }
+
+    console.log(module.getTimeRemainingMs());
+    document.querySelector('#indicator rect').setAttribute("fill", "grey");
+    isPressed = false;
+
 }
+
+function releaseHeldButton(isHeld){
+    if(!isHeld) return false;
+
+    let color_to_number = {
+        "blue": "4",
+        "white": "1",
+        "yellow": "5",
+    };
+
+    if(!Object.keys(color_to_number).includes(stripColor)) return releaseTime.includes("2");
+
+    return releaseTime.includes(color_to_number[stripColor]);
+}
+
 // Function to change the button color
 function changeButtonColor() {
-     const bgcolors = ["yellow","red","white","blue", "black"];
-     const colors = ["black", "white","black", "white", "white"];
+    const bgcolors = ["yellow","red","white","blue", "black"];
+    const colors = ["black", "white","black", "white", "white"];
+    const fillColors = ["yellow","blue","white","red"];
+    const arr = ["Press", "Hold", "Abort", "Detonate"];
 
-     let randomNumber = Math.floor(Math.random() * bgcolors.length) 
-     const button = document.getElementById("Knop");
+    let randomNumber = Math.floor(Math.random() * bgcolors.length) 
+    const button = document.getElementById("Knop");
+    bgColor = bgcolors[randomNumber];
+    stripColor = fillColors[randomNum];
+    text = arr[Math.floor(Math.random() * arr.length)];
 
      if (button){
-          button.style.backgroundColor = bgcolors[randomNumber];
-          button.style.color = colors[randomNumber];
-          button.style.border = "2px solid " + bgcolors[randomNumber];
-     
-          const arr = ["Press", "Hold", "Abort", "Detonate"];
-          const randomValue = arr[Math.floor(Math.random() * arr.length)];
-          button.textContent = randomValue;
+        button.style.backgroundColor = bgColor;
+        button.style.color = colors[randomNumber];
+        button.style.border = "2px solid " + bgcolors[randomNumber];
+        button.textContent = text;
      }
 }
 
